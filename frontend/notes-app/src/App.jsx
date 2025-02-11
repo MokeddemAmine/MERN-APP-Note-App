380f9ba177d9ed2dd6d85baccc0911d9a77946a6
Amine Mokeddem <mokeddemamine1707@gmail.com>
2025-02-11 07:50:23 +0100

    setup tailwindcss & react-router-dom

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import React from 'react'
 import Home from './pages/Home/Home'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Login from './pages/Login/LOgin';
+import SignUp from './pages/SignUp/SignUp';
 
+const routes = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home/>}/>
+      <Route path="/login" exact element={<Login/>}/>
+      <Route path="/signup" exact element={<SignUp/>}/>
+    </Routes>
+  </BrowserRouter>
+)
 function App() {
   return (
     <>
-     <Home/>
+     {routes}
     </>
   )
 }
